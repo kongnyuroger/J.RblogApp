af333375282f3c2b0dfbec8974d808108c2c26df
unknown <kongnyuroger711@gmail.com>
2024-08-17 15:04:03 +0100

    design desktop blogpage

--- src/components/blog-card/BlogCard.jsx
+++ src/components/blog-card/BlogCard.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import './BlogCard.css'
 
-const BlogCard = () => {
+const BlogCard = ({title, date, describetion}) => {
     return (
         <div className='BlogCard'>
-            <h1>BlogCard </h1>
+            <h1> {title? title : "UI Interaction of the week"}</h1>
+            <div className="info">
+                <p className="date">{date? date : "UI Interaction of the week"}</p>
+                <span>Express, Handlebars</span>
+            </div>
+            <p> {describetion? describetion: " Amet minim mollit non deserunt ullamco estsit aliqua dolor do amet sint. Velit officiaconsequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."}</p>
         </div>
     )
 }
--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import './Blog.css'
+import BlogCard from "../../blog-card/BlogCard"
 
 const Blog = () => {
     return (
-        <div className='page'>
-            <h1>Blog page</h1>
+        <div className='page blogpage'>
+            <h1 className="page-title">Blog</h1>
+            <BlogCard title='UI Interaction of the week'/>
+            <BlogCard />
+            <BlogCard />
+
         </div>
     )
 }
