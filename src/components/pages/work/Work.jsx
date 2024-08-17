d7739e12500f1dfd4830d4cb19a4e76c03a5024d
Joel Fombin <joelfombin@gmail.com>
2024-08-17 15:06:57 +0100

    First code for the work page

--- src/components/pages/work/Work.jsx
+++ src/components/pages/work/Work.jsx
@@ -1,10 +1,20 @@
-import React from "react"
-import './Work.css'
+import React from "react";
+import './Work.css';
+import WorkCard from "../../work-card/WorkCard";
+import { contents } from "./content";     // importing external file with an array of the work contents.
 
 const Work = () => {
     return (
         <div className='page'>
-            <h1>Work page</h1>
+            <h1 className="fw-bold">Work</h1>
+            
+            <div>
+                {
+                    contents.map((content) => (
+                        <WorkCard info={content} />  // setting each content object as a prop to the workcard component.
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- src/components/work-card/WorkCard.jsx
+++ src/components/work-card/WorkCard.jsx
@@ -1,12 +1,34 @@
-import React from "react"
-import './WorkCard.css'
+import React from "react";
+import './WorkCard.css';
+import { Container, Row, Col } from "react-bootstrap";
 
-const WorkCard = () => {
+const WorkCard = ({ info }) => { // Destructuring the info prop passed by the work parent.
     return (
         <div className='WorkCard'>
-            <h1>WorkCard </h1>
+            <Container fluid>
+                <Row>
+                    <Col className="d-flex flex-row bg-light px-3 py-4 border-bottom border-secondary border-2">
+
+                        {
+                        //div contains the picture.
+                        }
+
+                        <div className="photo bg-light"><img src={info.photo} className="img-fluid rounded" alt="" /></div>
+                        
+                        {
+                        // div contains the content.
+                        }
+
+                        <div className="content bg-light p-3">
+                            <h2>{info.heading}</h2>
+                            <span style={{color:"darkgray"}}><kbd>{info.year}</kbd> {info.worktype}</span>
+                            <p className="py-3">{info.text}</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
 
-export default WorkCard
+export default WorkCard;
