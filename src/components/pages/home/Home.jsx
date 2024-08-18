46918d6a1f34b50b8744b95252aca53e83202917
unknown <kongnyuroger711@gmail.com>
2024-08-18 13:36:03 +0100

    designed home page

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,51 @@
 import React from "react"
 import './Home.css'
+import { contents } from "../work/content"
+import BlogCard from "../../blog-card/BlogCard"
+import WorkCard from "../../work-card/WorkCard"
 
 const Home = () => {
+    const blogcostomStyle = {
+        border: "0px",
+        backgroundColor: "white",
+        padding: "2rem"
+    }
+    const workContents = contents.slice(-3)
     return (
-        <div className='page'>
-            <h1>Home page from texting branch</h1>
+        <div className='page home-page'>
+            <section className="sec1">
+                <div className="text">
+                    <h1>Hi, I am John <br></br>Creative Technologist</h1>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                    <button>Download Resumer</button>
+                </div>
+                <div className="img">
+                    <img src='images/profile.jpg'  alt="profileImg" />
+                </div>
+            </section>
+            <section className="sec2">
+                <p className="sec-heading">Recent Post</p>
+                <div className="blog-container">
+                    <BlogCard costomStyle={blogcostomStyle}
+                        title="Making a design system from scratch"
+                    />
+                    <BlogCard costomStyle={blogcostomStyle}
+                        title="Creating pixel perfect icons in Figma"
+                    />
+                </div>
+            </section>
+            <section className="sec3">
+                <p className="sec-heading">Featured works</p>
+                <div className="work-content">
+                {
+                    workContents.map((content) => (
+                        <WorkCard info={content} />  // setting each content object as a prop to the workcard component.
+                    ))
+                }
+                </div>
+            </section>
         </div>
+
     )
 }
 
